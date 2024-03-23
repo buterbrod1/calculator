b531fc1de867ce14f657fcf36fcb66dcf4575b01
buterbrod1 <164636400+buterbrod1@users.noreply.github.com>
2024-03-23 17:24:04 +0400

    Добавьте файлы проекта.

---
+++ calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstVa, secondVa;
+            string action;
+
+            Console.WriteLine("Введите число 1:");
+            firstVa = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2:");
+            secondVa = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберете операцию:");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(firstVa + secondVa);
+                    break;
+                case "-":
+                    Console.WriteLine(firstVa - secondVa);
+                    break;
+                case "*":
+                    Console.WriteLine(firstVa * secondVa);
+                    break;
+                case "/":
+                    Console.WriteLine(firstVa / secondVa);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
